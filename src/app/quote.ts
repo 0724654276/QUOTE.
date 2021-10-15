45d52cd8a265a5056bb596e6aba9fea3ceb91a07
0724654276 <you@example.com>
2021-10-15 15:04:27 +0300

    imported class quote into quote component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,4 @@
-export class Quote (public id:number, public quote: string, public author: string, public publisher: string){
-    
+export class Quote  constructor(public id:number, public quote: string, public author: string, public publisher: string){
+
 } 
 
