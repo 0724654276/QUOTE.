1923a76638b1d9bc45727728afb341dffe0e1727
0724654276 <you@example.com>
2021-10-15 16:02:48 +0300

    added toggle

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,13 @@
-export class Quote  constructor(public id:number, public quote: string, public author: string, public publisher: string){
-
-} 
+export class Quote {
+    showDetail: boolean
+    postDate!: Date
+    upVote: number
+    downVote: number
+    constructor(public id:number, public quote: string, public author: string, public publisher: string) {
+        this.showDetail = false
+        this.postDate = new Date()
+        this.upVote= 0
+        this.downVote= 0
+    }
+}
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,6 +14,47 @@ export class QuoteComponent implements OnInit {
     new Quote(3,'Ruby is rubbish! PHP is phpantastic!', 'Nikita Popov'),
     new Quote(4,'Code is like humor. When you have to explain it, it’s bad.','Cory House', 
   ]
+  toggleDetails(index: number) {
+    this.quotes[index].showDetail = !this.quotes[index].showDetail;
+  }
+
+  addNewQuote(quote: Quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    this.quotes.push(quote)
+ 
+  }
+
+  addUpvote(index: number) {
+    this.quotes[index].upVote += 1
+  }
+
+  addDownvote(index: number) {
+    this.quotes[index].downVote += 1
+  }
+
+  get sortByLikes()
+  {
+     return this.quotes.sort((q1, q2) => {
+      return q2.upVote - q1.upVote;
+    })
+  } 
+
+  quoteDelete(isDeleteQuote: boolean, index: number) {
+    if (isDeleteQuote) {
+      let toDelete = confirm(`This will remove ${this.quotes[index].publisher}'s quote. Proceed?`)
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
+
 
 
   constructor() { }
