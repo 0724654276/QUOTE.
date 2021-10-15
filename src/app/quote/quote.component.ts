776b5f3c15733c0aa4c3a191ef4cf4a1ac1cfdb0
0724654276 <you@example.com>
2021-10-15 15:16:52 +0300

    created property in quote.component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,6 +8,14 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(1,'Any fool can write code that a computer can understand. Good programmers write code that humans can understand', 'Martin Fowler'),
+    new Quote(2,'First, solve the problem. Then, write the code, 'John Johnson'),
+    new Quote(3,'Ruby is rubbish! PHP is phpantastic!', 'Nikita Popov'),
+    new Quote(4,'Code is like humor. When you have to explain it, it’s bad.', 'Cory House', 
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
