d2bb44ab14785e5a46e6dc636472e2e51c01f0ab
0724654276 <you@example.com>
2021-10-16 11:53:11 +0300

    added logic to form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { FormComponent } from './form/form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
-    FormComponent
+    FormComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
